d09c97f46b7ae8a704e07af2d8aa6ae88c459ef5
RFG <raul.ferrer.dev@gmail.com>
2019-12-29 14:40:37 +0100

    Initial Commit

---
+++ GHFollowers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GHFollowers
+//
+//  Created by RaulF on 29/12/2019.
+//  Copyright © 2019 ImTech. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
